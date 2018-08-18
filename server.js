baf8d62109ecf846690863f9aa9cb488ab52c1cc
Andrew <andrewmd4774@gmail.com>
2018-08-18 12:25:16 -0400

    fuck you

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bp = require("body-parser");
 var path = require("path");
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 app.use(bp.urlencoded({ extended: true }));
 app.use(bp.json());
